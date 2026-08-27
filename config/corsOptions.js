24c7af3d16a542499ceb7fa48c664db931347003
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-08-26 23:56:48 -0400

    chore: remove console logs

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,5 @@
 const { allowedOrigins } = require('./allowedOrigins');
 
-console.log(allowedOrigins);
-
 module.exports.corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
