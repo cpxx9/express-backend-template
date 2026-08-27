d0b5cf1fe4f15dd50a00dc1a963bd45f406826b5
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-08-27 00:05:06 -0400

    fix: check if extra origins are passed, if not do not crash

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 module.exports.allowedOrigins = [
   'http://localhost:3001',
   'http://127.0.0.1:3001',
-  ...process.env.EXTRA_ORIGINS.split(',')
+  ...(process.env.EXTRA_ORIGINS ? process.env.EXTRA_ORIGINS.split(',') : '')
 ];
