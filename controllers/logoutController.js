46ddc0662ac6a68a33a8e1d835e674cdffc29765
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-08-26 20:32:33 -0400

    refactor: clean up logoutController to work with new password and cookie changes

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,29 +1,33 @@
 const { prisma } = require('../lib/prisma');
+const { refreshCookieOptions } = require('../config/cookieOptions');
 
 const logoutController = async (req, res, next) => {
   // On frontend, delete the access token from memory
   const { cookies } = req;
   if (!cookies?.jwt) return res.sendStatus(204);
-  const refreshToken = cookies.jwt.split(' ')[1];
+
+  const refreshToken = cookies.jwt;
+
   try {
     const user = await prisma.user.findFirst({
       where: {
         refresh: refreshToken
       }
     });
-    if (!user) {
-      res.clearCookie('jwt', { httpOnly: true });
-      return res.sendStatus(204);
+
+    res.clearCookie('jwt', refreshCookieOptions);
+
+    if (user) {
+      await prisma.user.update({
+        where: {
+          id: user.id
+        },
+        data: {
+          refresh: ''
+        }
+      });
     }
-    await prisma.user.update({
-      where: {
-        id: user.id
-      },
-      data: {
-        refresh: ''
-      }
-    });
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // add secure: true - https
+
     res.sendStatus(204);
   } catch (err) {
     return next(err);
