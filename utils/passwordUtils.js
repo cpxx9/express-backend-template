cd83fd4041de36b1dbec04b4eebbd06176872570
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-08-29 21:41:32 -0400

    feat: helper funtion to create hash of tokens

--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -50,8 +50,13 @@ function issueJWT(user) {
   };
 }
 
+function hashToken(token) {
+  return crypto.createHash('sha256').update(token).digest('hex');
+}
+
 module.exports = {
   validPassword,
   genPassword,
-  issueJWT
+  issueJWT,
+  hashToken
 };
