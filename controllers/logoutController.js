7d332ebd692c9c1eb48d7cb7426b9ec7bebc6329
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-03 19:18:53 -0400

    refactor: use async handler

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,25 +1,18 @@
+const asyncHandler = require('express-async-handler');
 const { prisma } = require('../lib/prisma');
 const { baseCookieOptions } = require('../config/cookieOptions');
 const { hashToken } = require('../utils/passwordUtils');
 
-const logoutController = async (req, res, next) => {
-  // On frontend, delete the access token from memory
+const logoutController = asyncHandler(async (req, res) => {
   const { cookies } = req;
   if (!cookies?.jwt) return res.sendStatus(204);
 
-  const refreshToken = cookies.jwt;
+  await prisma.refreshToken.deleteMany({
+    where: { token: hashToken(cookies.jwt) }
+  });
 
-  try {
-    await prisma.refreshToken.deleteMany({
-      where: { token: hashToken(refreshToken) }
-    });
-
-    res.clearCookie('jwt', baseCookieOptions);
-
-    res.sendStatus(204);
-  } catch (err) {
-    return next(err);
-  }
-};
+  res.clearCookie('jwt', baseCookieOptions);
+  res.sendStatus(204);
+});
 
 module.exports = { logoutController };
