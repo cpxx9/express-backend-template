2e972c50c4bbc8e51ebfad17d1f5b3c93458808a
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-03 19:25:28 -0400

    refactor: use async handler

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,85 +1,54 @@
+const asyncHandler = require('express-async-handler');
 const { prisma } = require('../lib/prisma');
+const CustomNotFoundError = require('../errors/CustomNotFoundError');
 
-const listUsers = async (req, res, next) => {
-  try {
-    const users = await prisma.user.findMany({
-      select: {
-        created: true,
-        updated: true,
-        username: true,
-        email: true,
-        firstname: true,
-        lastname: true
-        // posts: {
-        //   include: { authorId: false }
-        // },
-        // comments: true
-      }
-    });
-    res.status(200).json({ success: true, data: users });
-  } catch (err) {
-    next(err);
-  }
+const userSelect = {
+  created: true,
+  updated: true,
+  username: true,
+  email: true,
+  firstname: true,
+  lastname: true
 };
 
-const listUser = async (req, res, next) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: req.params.userId
-      },
-      select: {
-        created: true,
-        updated: true,
-        username: true,
-        email: true,
-        firstname: true,
-        lastname: true
-        // posts: true,
-        // comments: true
-      }
-    });
+const listUsers = asyncHandler(async (req, res) => {
+  const users = await prisma.user.findMany({ select: userSelect });
+  res.status(200).json({ success: true, data: users });
+});
 
-    if (!user) {
-      return res.status(404).json({ success: false, msg: 'User not found!' });
-    }
+const listUser = asyncHandler(async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.params.userId },
+    select: userSelect
+  });
 
-    res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    next(err);
-  }
-};
+  if (!user) throw new CustomNotFoundError('User not found');
 
-const updateUser = async (req, res, next) => {
+  res.status(200).json({ success: true, data: user });
+});
+
+const updateUser = asyncHandler(async (req, res) => {
   const { firstname, lastname, email } = req.body;
   const data = {};
   if (firstname !== undefined) data.firstname = firstname;
   if (lastname !== undefined) data.lastname = lastname;
   if (email !== undefined) data.email = email;
 
-  try {
-    const user = await prisma.user.update({
-      where: { id: req.params.userId },
-      data,
-      omit: { hash: true }
-    });
-    res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    next(err);
-  }
-};
+  const user = await prisma.user.update({
+    where: { id: req.params.userId },
+    data,
+    omit: { hash: true }
+  });
+  res.status(200).json({ success: true, data: user });
+});
 
-const deleteUser = async (req, res, next) => {
-  try {
-    const deletedUser = await prisma.user.delete({
-      where: { id: req.params.userId },
-      omit: { hash: true }
-    });
+const deleteUser = asyncHandler(async (req, res) => {
+  const deletedUser = await prisma.user.delete({
+    where: { id: req.params.userId },
+    omit: { hash: true }
+  });
 
-    res.status(200).json({ success: true, data: deletedUser });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, data: deletedUser });
+});
 
 module.exports = { listUsers, listUser, updateUser, deleteUser };
