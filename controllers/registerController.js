e21f6cb88b274b23ab4453489d30f890db020bee
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-03 19:07:14 -0400

    refactor: use handleValidation in controller

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,51 +1,44 @@
-const { validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
 const { prisma } = require('../lib/prisma');
 const { genPassword, issueJWT, hashToken } = require('../utils/passwordUtils');
 const { validateUser } = require('../utils/validations');
+const { handleValidation } = require('../middleware/handleValidation');
 const { refreshCookieOptions } = require('../config/cookieOptions');
 
 const postNewUser = [
   validateUser,
-  async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ success: false, errors: errors.array() });
-    }
-
+  handleValidation,
+  asyncHandler(async (req, res) => {
     const { hash } = genPassword(req.body.password);
 
-    try {
-      const user = await prisma.user.create({
-        data: {
-          username: req.body.username.toLowerCase().trim(),
-          email: req.body.email,
-          firstname: req.body.firstname,
-          lastname: req.body.lastname,
-          hash
-        }
-      });
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username.toLowerCase().trim(),
+        email: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        hash
+      }
+    });
 
-      const { refreshToken, accessToken } = issueJWT(user);
+    const { refreshToken, accessToken } = issueJWT(user);
 
-      await prisma.refreshToken.create({
-        data: {
-          token: hashToken(refreshToken.token),
-          userId: user.id,
-          expiresAt: refreshToken.expiresAt
-        }
-      });
+    await prisma.refreshToken.create({
+      data: {
+        token: hashToken(refreshToken.token),
+        userId: user.id,
+        expiresAt: refreshToken.expiresAt
+      }
+    });
 
-      res.cookie('jwt', refreshToken.token, refreshCookieOptions);
+    res.cookie('jwt', refreshToken.token, refreshCookieOptions);
 
-      res.status(201).json({
-        success: true,
-        token: accessToken.token,
-        expiresIn: accessToken.expires
-      });
-    } catch (err) {
-      return next(err);
-    }
-  }
+    res.status(201).json({
+      success: true,
+      token: accessToken.token,
+      expiresIn: accessToken.expires
+    });
+  })
 ];
 
 module.exports = {
