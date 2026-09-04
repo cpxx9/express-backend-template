957733a7713a0192733bd5653008e44616fd1de4
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-03 22:49:55 -0400

    pre ts-jest switch

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,12 @@ module.exports = {
   setupFiles: ['<rootDir>/__tests__/setup/loadEnv.js'],
   globalSetup: '<rootDir>/__tests__/setup/globalSetup.js',
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup/setupAfterEnv.js'],
-  testMatch: ['**/__tests__/**/*.test.js']
+  testMatch: ['**/__tests__/**/*.test.js'],
+  transform: {
+    '^.+\\.(ts|js)$': [
+      'ts-jest',
+      { tsconfig: 'tsconfig.json', isolateModules: true }
+    ]
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node']
 };
---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const ACCESS_EXP = '15m';
+export const REFRESH_EXP = '1d';
+export const REFRESH_EXP_MS = 24 * 60 * 60 * 1000;
+
+export const LOGIN_FAIL_THRESHOLD = 5;
+export const LOGIN_BACKOFF_BASE_MS = 15 * 1000;
+export const LOGIN_BACKOFF_MAX_MS = 15 * 60 * 1000;
