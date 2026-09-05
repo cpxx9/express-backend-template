814f41550a4bad03d0620f07dc8bd8b2143ff099
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-05 14:19:02 -0400

    refactor: convert to ts

--- src/config/cookieOptions.ts
+++ src/config/cookieOptions.ts
@@ -1,18 +1,16 @@
-const { REFRESH_EXP_MS } = require('../lib/constants');
+import { REFRESH_EXP_MS } from '../lib/constants';
 
-const isDevOrTest =
+export const isDevOrTest =
   process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development';
 
-const baseCookieOptions = {
+export const baseCookieOptions = {
   httpOnly: true,
   sameSite: !isDevOrTest ? 'None' : 'Lax',
   secure: !isDevOrTest,
   path: '/'
 };
 
-const refreshCookieOptions = {
+export const refreshCookieOptions = {
   ...baseCookieOptions,
   maxAge: REFRESH_EXP_MS
 };
-
-module.exports = { baseCookieOptions, refreshCookieOptions };
