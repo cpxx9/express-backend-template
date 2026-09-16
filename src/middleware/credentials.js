21ce0bd5e4c514d94652c4046099795ce121a842
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-15 20:42:05 -0500

    refactor: convert to ts

--- src/middleware/credentials.js
+++
@@ -1,9 +0,0 @@
-const { allowedOrigins } = require('../config/allowedOrigins');
-
-module.exports.credentials = (req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  next();
-};
