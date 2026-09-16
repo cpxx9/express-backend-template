179b7e3a63555d10e877223e34280d25a8f0c9fe
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-16 17:08:35 -0400

    refactor: convert to ts

---
+++ src/routes/indexRouter.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { loginRouter } from './loginRouter';
+import { registerRouter } from './registerRouter';
+import { refreshRouter } from './refreshRouter';
+import { logoutRouter } from './logoutRouter';
+import { usersRouter } from './usersRouter';
+
+const indexRouter = Router();
+
+indexRouter.use('/register', registerRouter);
+indexRouter.use('/login', loginRouter);
+indexRouter.use('/users', usersRouter);
+indexRouter.use('/refresh', refreshRouter);
+indexRouter.use('/logout', logoutRouter);
+
+export = {
+  indexRouter
+};
