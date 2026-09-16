bc6683d39578ff82c32b2766fbe702b45b0899bd
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-09-15 20:59:15 -0500

    refactor: convert to ts

--- src/utils/backoff.ts
+++ src/utils/backoff.ts
@@ -1,12 +1,10 @@
-const {
+import {
   LOGIN_BACKOFF_BASE_MS,
   LOGIN_BACKOFF_MAX_MS,
   LOGIN_FAIL_THRESHOLD
-} = require('../lib/constants');
+} from '../lib/constants';
 
-function backoffMs(failedAttempts) {
+export function backoffMs(failedAttempts: number) {
   const over = failedAttempts - LOGIN_FAIL_THRESHOLD;
   return Math.min(LOGIN_BACKOFF_BASE_MS * 2 ** over, LOGIN_BACKOFF_MAX_MS);
 }
-
-module.exports = { backoffMs };
